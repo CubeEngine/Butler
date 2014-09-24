69bce2af096f0416e81a837c70cd8d0fb7f171b8
Anselm <Faithcaio@yahoo.de>
2014-09-24 14:38:51 +0200

    some documentation

--- src/main/java/de/cubeisland/engine/command/methodic/context/ParameterizedContext.java
+++ src/main/java/de/cubeisland/engine/command/methodic/context/ParameterizedContext.java
@@ -22,12 +22,6 @@
  */
 package de.cubeisland.engine.command.methodic.context;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-
 import de.cubeisland.engine.command.parameter.FlagParameter;
 import de.cubeisland.engine.command.parameter.Parameter;
 import de.cubeisland.engine.command.parameter.ParsedParameter;
@@ -36,6 +30,15 @@ import de.cubeisland.engine.command.parameter.property.FixedPosition;
 import de.cubeisland.engine.command.parameter.property.FixedValues;
 import de.cubeisland.engine.command.tokenized.TokenizedInvocation;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+/**
+ * A context with parameterized values
+ */
 public class ParameterizedContext extends BaseCommandContext
 {
     private final Map<Integer, ParsedParameter> positional = new HashMap<>();
@@ -51,7 +54,7 @@ public class ParameterizedContext extends BaseCommandContext
             Parameter parameter = parsed.getParameter();
             if (parameter instanceof FlagParameter)
             {
-                flags.put(((FlagParameter)parameter).name(), parsed);
+                flags.put(((FlagParameter) parameter).name(), parsed);
             }
             else
             {
@@ -60,7 +63,7 @@ public class ParameterizedContext extends BaseCommandContext
                 if (fixedValues == null && pos == null)
                 {
                     throw new IllegalStateException(
-                        "Cannot assign parameter that is neither positional nor has fixed values");
+                            "Cannot assign parameter that is neither positional nor has fixed values");
                 }
                 if (fixedValues != null)
                 {
@@ -74,21 +77,42 @@ public class ParameterizedContext extends BaseCommandContext
         }
     }
 
+    /**
+     * Returns whether the context has any positional parameters
+     *
+     * @return whether the context has any positional parameters
+     */
     public final boolean hasPositional()
     {
         return !this.positional.isEmpty();
     }
 
+    /**
+     * Returns whether the context has a positional parameter for given index
+     *
+     * @param index the index
+     * @return whether the context has a positional parameter for given index
+     */
     public final boolean hasPositional(int index)
     {
         return this.positional.containsKey(index);
     }
 
+    /**
+     * Returns the amount of positional parameters
+     *
+     * @return the amount of positional parameters
+     */
     public final int getPositionalCount()
     {
         return this.positional.size();
     }
 
+    /**
+     * Returns the unparsed positional parameters as {@code List<String>}
+     *
+     * @return the unparsed positional parameters as {@code List<String>}
+     */
     public final List<String> getRawPositional()
     {
         List<String> result = new ArrayList<>();
@@ -99,6 +123,12 @@ public class ParameterizedContext extends BaseCommandContext
         return result;
     }
 
+    /**
+     * Returns the unparsed positional parameter for given index
+     *
+     * @param index the index
+     * @return the unparsed positional parameter for given index
+     */
     public final String getString(int index)
     {
         ParsedParameter parsed = this.positional.get(index);
@@ -109,6 +139,13 @@ public class ParameterizedContext extends BaseCommandContext
         return null;
     }
 
+    /**
+     * Returns the unparsed positional parameter for given index or the default value if not found
+     *
+     * @param index the index
+     * @param def   the default value
+     * @return the unparsed positional parameter for given index or {@code def} if not found
+     */
     public final String getString(int index, String def)
     {
         String result = this.getString(index);
@@ -119,6 +156,12 @@ public class ParameterizedContext extends BaseCommandContext
         return result;
     }
 
+    /**
+     * Returns the unparsed positional parameters joined by {@code " "} starting with given index until the last
+     *
+     * @param from the index to start at
+     * @return the unparsed positional parameters starting with given index
+     */
     public final String getStrings(int from)
     {
         if (!this.hasPositional(from))
@@ -133,16 +176,33 @@ public class ParameterizedContext extends BaseCommandContext
         return sb.toString();
     }
 
+    /**
+     * Returns whether the context has any named parameters
+     *
+     * @return whether the context has any named parameters
+     */
     public final boolean hasNamed()
     {
         return !this.nameBased.isEmpty();
     }
 
+    /**
+     * Returns whether the context has a named parameter for given name
+     *
+     * @param name the name
+     * @return whether the context has a named parameter for given name
+     */
     public final boolean hasNamed(String name)
     {
         return this.nameBased.containsKey(name);
     }
 
+    /**
+     * Returns the unparsed named parameter for given name
+     *
+     * @param name the name
+     * @return the unparsed named parameter for given name
+     */
     public final String getString(String name)
     {
         ParsedParameter parsed = this.nameBased.get(name);
@@ -153,6 +213,13 @@ public class ParameterizedContext extends BaseCommandContext
         return null;
     }
 
+    /**
+     * Returns the unparsed named parameter for given name or the default value if not found
+     *
+     * @param name the name
+     * @param def  the default value
+     * @return the unparsed named parameter for given name or {@code def} if not found
+     */
     public final String getString(String name, String def)
     {
         String result = this.getString(name);
@@ -163,11 +230,21 @@ public class ParameterizedContext extends BaseCommandContext
         return result;
     }
 
+    /**
+     * Returns the amount of named parameters
+     *
+     * @return the amount of named parameters
+     */
     public final int getNamedCount()
     {
         return this.nameBased.size();
     }
 
+    /**
+     * Returns the unparsed named parameters as {@code Map<String,String>}
+     *
+     * @return the unparsed named parameters
+     */
     public final Map<String, String> getRawNamed()
     {
         Map<String, String> map = new HashMap<>();
@@ -178,16 +255,33 @@ public class ParameterizedContext extends BaseCommandContext
         return map;
     }
 
+    /**
+     * Returns whether the context has any flags
+     *
+     * @return whether the context has any flags
+     */
     public final boolean hasFlags()
     {
         return !this.flags.isEmpty();
     }
 
+    /**
+     * Returns whether the context has the flag
+     *
+     * @param name the name of the flag to check
+     * @return whether the context has the flag
+     */
     public final boolean hasFlag(String name)
     {
         return this.flags.containsKey(name);
     }
 
+    /**
+     * Returns whether the context has all the flags given
+     *
+     * @param names the names of the flags to check
+     * @return whether the context has the flags
+     */
     public final boolean hasFlags(String... names)
     {
         for (String name : names)
@@ -200,21 +294,41 @@ public class ParameterizedContext extends BaseCommandContext
         return true;
     }
 
+    /**
+     * Returns the amount of flags this context has
+     *
+     * @return the amount of flags this context has
+     */
     public final int getFlagCount()
     {
         return this.flags.size();
     }
 
+    /**
+     * Returns the parsed positional parameter at given index
+     *
+     * @param index the index
+     * @param <T>   the Type of the parsed parameter
+     * @return the parsed positional parameter
+     */
     @SuppressWarnings("unchecked")
     public final <T> T get(int index)
     {
         if (this.hasPositional(index))
         {
-            return (T)this.positional.get(index);
+            return (T) this.positional.get(index);
         }
         return null;
     }
 
+    /**
+     * Returns the parsed positional parameter at given index or the default value if not found
+     *
+     * @param index the index
+     * @param def   the default value
+     * @param <T>   the Type of the parsed parameter
+     * @return the parsed positional parameter at given index or {@code def} if not found
+     */
     public final <T> T get(int index, T def)
     {
         T result = this.get(index);
@@ -225,16 +339,31 @@ public class ParameterizedContext extends BaseCommandContext
         return result;
     }
 
+    /**
+     * Returns the parsed named parameter for given name
+     *
+     * @param name the name
+     * @param <T>  the Type of the parsed parameter
+     * @return the parsed named parameter for given name
+     */
     @SuppressWarnings("unchecked")
     public final <T> T get(String name)
     {
         if (this.hasNamed(name))
         {
-            return (T)this.nameBased.get(name);
+            return (T) this.nameBased.get(name);
         }
         return null;
     }
-    
+
+    /**
+     * Returns the parsed named parameter for given name or the default value if not found
+     *
+     * @param name the name
+     * @param def  the default value
+     * @param <T>  the Type of the parsed parameter
+     * @return the parsed named parameter for the given name or {@code def} if not found
+     */
     public final <T> T get(String name, T def)
     {
         T result = this.get(name);
