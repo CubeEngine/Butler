242d1e9d81940c2fce21ba602266ecd8dcc58c85
Phillip Schichtel <phillip.public@schich.tel>
2017-05-24 19:35:35 +0200

    update all the maven plugins

--- src/main/java/org/cubeengine/butler/ProviderManager.java
+++ src/main/java/org/cubeengine/butler/ProviderManager.java
@@ -1,6 +1,6 @@
-/**
+/*
  * The MIT License
- * Copyright (c) 2014 Cube Island
+ * Copyright © 2014 Cube Island
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
